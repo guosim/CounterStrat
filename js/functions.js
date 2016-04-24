6605849b8d6985c4d517b112d0917e926c91bb55
Henry <projectsoricidae@gmail.com>
2016-04-23 23:10:01 -0700

    linked box with map, hides map onclick

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,37 @@
 $(document).ready(function() {
 
-var $numBox = 0;
+var $num = 0;
 	$("#select-map").change(
 		function() {
 			console.log('map changed');
 			$('.map').attr('src','images/' + $('#select-map').val() + '.jpg');
 	});
 
-	$('#create').click(
+	$("#create").click(
 		function() {
-			$numBox = $numBox + 1;
-			$('<div class="node"><div class="textbox" contentEditable="true"> #' + $numBox + ' Edit here!</div></div>').appendTo('#left');
-			$(".textbox").resizable().draggable();
-	  });
-
-
+			$num = $num + 1;
+			$('<div class="node' + $num + '"><div class="textbox box' + $num + '" contentEditable="true"> #' + $num + ' Edit here!</div></div>').appendTo('#left').resizable().draggable();
+			$('.box'+$num)
+				.resizable()
+				.draggable()
+				.click(function() {
+					if($(this).is('.ui-draggable-dragging')){
+						return;
+					};
+					$("canvas").css("display", "none");
+					var nodeClass = $(this).parent().prop("className").split(" ");
+					console.log(nodeClass[0]);
+					$("canvas").each(
+						function() {
+							var canvasClass = $(this).prop("className");
+							if(nodeClass[0] === canvasClass) {
+								$(this).css("display", "inline");
+								//console.log($(this).css("display"));
+							}
+						});
+					//if canvas has same class as this's parent, show canvas
+				});
+			$('<canvas class="node' + $num + '"></canvas>').appendTo("#right");
+			$("canvas").css("display", "none");
+	});
 });
